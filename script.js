fb19a9309bf28fb932d718639104d74ad77bbbaa
jstam89 <jer.stam89@gmail.com>
2020-05-12 11:46:15 +0200

    switch to jsonp fr cors

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 import "https://api.mapbox.com/mapbox-gl-js/v1.8.1/mapbox-gl.js"
 import "https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.5.1/mapbox-gl-geocoder.min.js"
-import jsonp from "jsonp"
 
 window.addEventListener('DOMContentLoaded', (event) => {
 
@@ -9,33 +8,35 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/dark-v10',
-        center: [5.86508001, 52.1327033],
+        center: [5.86508001, 52.1327033], //the Netherlands
         zoom: 6.5
     });
 
     map.on('load', () => {
 
-        jsonp("https://api.routeradar.nl/api/v1/reports?query_type=overview", (error, data) => {
-            if (error) {
-                console.log('Looks like there was a problem. Status Code: ' + error);
-            }
-            let point = data.map(key => key.point.coordinates)
-
-            point.forEach(point => {
-                let el = document.createElement('div');
-                el.className = 'marker';
-
-                new mapboxgl.Marker(el)
-                    .setLngLat(point)
-                    .addTo(map);
-            });
-
-            map.addControl(
-                new MapboxGeocoder({
-                    accessToken: mapboxgl.accessToken,
-                    mapboxgl: mapboxgl
-                })
-            );
+        fetch('https://api.routeradar.nl/api/v1/reports?query_type=overview',{
+            mode: "no-cors"
         })
+            .then((response) => {
+                if (response.status !== 200) {
+                    console.log('Looks like there was a problem. Status Code: ' + response.status);
+                    return
+                }
+                response.json()
+                    .then(data => {
+                        let point = data.map(key => key.point.coordinates)
+
+                        point.forEach(point => {
+                            let el = document.createElement('div');
+                            el.className = 'marker';
+
+                            new mapboxgl.Marker(el)
+                                .setLngLat(point)
+                                .addTo(map);
+                        })
+                    }).catch((error) => {
+                    console.log(error)
+                })
+            })
     });
 });
